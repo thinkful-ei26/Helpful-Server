e2a77cb99dabf2bded2c3bd1e52c5273622b628f
CodeyWorley <codeyworley@gmail.com>
2019-02-14 07:13:59 -0700

    added routing for rsvp

--- routes/rsvp.js
+++ routes/rsvp.js
@@ -7,7 +7,7 @@ const Rsvp = require("../models/rsvp");
 const router = express.Router();
 
 /* Get All Rsvps Endpoint  */
-
+// get all rsvp's
 router.get("/all", (req, res, next) => {
   /* Validation */
 
@@ -22,8 +22,46 @@ router.get("/all", (req, res, next) => {
     });
 });
 
-/* Get Single Rsvp Endpoint  */
+// get all rsvp's for a user
+router.get("/user/:userId", (req, res, next) => {
+  const userId = req.params.userId;
+  /* Validation */
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error("The `userId` is not valid");
+    err.status = 400;
+    return next(err);
+  } 
+  Rsvp.find({userId})
+    .sort({ createdAt: "desc" })
+    .then(rsvps => {
+      res.json(rsvps);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
 
+// get all rsvp's for an event
+router.get("/event/:eventId", (req, res, next) => {
+  const eventId = req.params.eventId;
+  /* Validation */
+  if (!mongoose.Types.ObjectId.isValid(eventId)) {
+    const err = new Error("The `eventId` is not valid");
+    err.status = 400;
+    return next(err);
+  } 
+  Rsvp.find({eventId})
+    .sort({ createdAt: "desc" })
+    .then(rsvps => {
+      res.json(rsvps);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* Get Single Rsvp Endpoint  */
+// get rsvp by id
 router.get("/:id", (req, res, next) => {
   const id = req.params.id;
   /* Validation */
@@ -32,11 +70,28 @@ router.get("/:id", (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-  /*            */
   Rsvp.findOne({ _id: id })
     .sort({ createdAt: "desc" })
     .then(rsvps => {
       res.json(rsvps);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// get rsvp by userId and eventId
+router.get("/:userId/:eventId", (req, res, next) => {
+  const {userId, eventId} = req.params;
+  /* Validation */
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error("The `user id` is not valid");
+    err.status = 400;
+    return next(err);
+  }
+  Rsvp.findOne({ userId, eventId })
+    .then(rsvp => {
+      res.json(rsvp);
     })
     .catch(err => {
       next(err);
@@ -113,6 +168,25 @@ router.delete("/", (req, res, next) => {
   }
   /*            */
   Rsvp.findOneAndDelete({ _id: id })
+    .then(rsvp => {
+      res.json(rsvp);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+// delete rsvp by user id and event id
+router.delete("/user", (req, res, next) => {
+  const {userId, eventId} = req.body;
+  /* Validation */
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error("The `id` is not valid");
+    err.status = 400;
+    return next(err);
+  }
+  /*            */
+  Rsvp.findOneAndDelete({ userId, eventId })
     .then(rsvp => {
       res.json(rsvp);
     })
