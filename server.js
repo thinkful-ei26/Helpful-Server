16b35dd85379dfeff71ba67f1908adbb14a00481
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-20 09:01:32 -0700

    add and load commentsRouter

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 
-const organizationRouter = require("./routes/organization");
-const followRouter = require("./routes/following");
-const eventRouter = require("./routes/event");
-const roleRouter = require("./routes/role");
-const rsvpRouter = require("./routes/rsvp");
-const meetupRouter = require("./routes/rsvpmeetup");
-const { router: usersRouter } = require("./users");
-const { router: authRouter, localStrategy, jwtStrategy } = require("./auth");
+const organizationRouter = require('./routes/organization');
+const followRouter = require('./routes/following');
+const eventRouter = require('./routes/event');
+const roleRouter = require('./routes/role');
+const rsvpRouter = require('./routes/rsvp');
+const meetupRouter = require('./routes/rsvpmeetup');
+const { router: usersRouter } = require('./users');
+const { router: authRouter, localStrategy, jwtStrategy } = require('./auth');
 
 mongoose.Promise = global.Promise;
 
@@ -24,7 +24,7 @@ const app = express();
 app.use(morgan('common'));
 
 // CORS
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
@@ -38,15 +38,16 @@ app.use(express.json());
 passport.use(localStrategy);
 passport.use(jwtStrategy);
 
-app.use("/users/", usersRouter);
-app.use("/auth/", authRouter);
-app.use("/org", organizationRouter);
-app.use("/event", eventRouter);
-app.use("/follow", followRouter);
-app.use("/rsvp", rsvpRouter);
-app.use("/meetup", meetupRouter);
-app.use("/role", roleRouter);
-const jwtAuth = passport.authenticate("jwt", { session: false });
+app.use('/users/', usersRouter);
+app.use('/auth/', authRouter);
+app.use('/org', organizationRouter);
+app.use('/event', eventRouter);
+app.use('/follow', followRouter);
+app.use('/rsvp', rsvpRouter);
+app.use('/meetup', meetupRouter);
+app.use('/role', roleRouter);
+app.use('comments', commentsRouter);
+const jwtAuth = passport.authenticate('jwt', { session: false });
 
 // A protected endpoint which needs a valid JWT to access it
 app.get('/api/protected', jwtAuth, (req, res) => {
