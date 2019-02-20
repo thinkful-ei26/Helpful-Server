d5a3571a531a1ab4d0c866bc71231eab5fb5001c
Joaquin Fox <joaquin.fox@gmail.com>
2019-02-20 10:19:30 -0700

    applied use-strict to backend files, created comments file

--- models/orgrating.js
+++ models/orgrating.js
@@ -1,26 +1,27 @@
+'use strict';
 /* Goal is to have this model represent users rating organizations */
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const orgRatingSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    rating: { type: Number, required: true },
-    description: { type: String, required: true },
-    organizationId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Organization",
-        required: true
-    }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  rating: { type: Number, required: true },
+  description: { type: String, required: true },
+  organizationId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Organization',
+    required: true
+  }
 });
 
-orgRatingSchema.set("timestamps", true);
+orgRatingSchema.set('timestamps', true);
 orgRatingSchema.index({ userId: 1, organizationId: 1 }, { unique: true });
-orgRatingSchema.set("toJSON", {
-    virtuals: true,
-    transform: (doc, result) => {
-        delete result._id;
-        delete result.__v;
-    }
+orgRatingSchema.set('toJSON', {
+  virtuals: true,
+  transform: (doc, result) => {
+    delete result._id;
+    delete result.__v;
+  }
 });
 
-module.exports = mongoose.model("Orgrating", orgRatingSchema);
+module.exports = mongoose.model('Orgrating', orgRatingSchema);
